966ab2e1519c9e38dd4b1e2cd03d24f87b7e6d36
Rob Crowe <hello@vivalacrowe.com>
2013-01-23 10:55:08 +0000

    Docblocks and tidy up

--- src/TwigBridge/Console/CompileCommand.php
+++ src/TwigBridge/Console/CompileCommand.php
@@ -1,11 +1,21 @@
 <?php
 
+/**
+ * Brings Twig to Laravel.
+ *
+ * @author Rob Crowe <hello@vivalacrowe.com>
+ * @license MIT
+ */
+
 namespace TwigBridge\Console;
 
 use Illuminate\Console\Command;
 use Symfony\Component\Finder\Finder;
 use TwigBridge\TwigBridge;
 
+/**
+ * Pre-compile Twig templates so it doesn't have to be done at runtime.
+ */
 class CompileCommand extends Command
 {
     /**
@@ -34,14 +44,13 @@ class CompileCommand extends Command
      */
     public function fire()
     {
-        $view_paths      = $this->laravel['view']->getFinder()->getPaths();
-        $namespace_paths = $this->laravel['view']->getFinder()->getHints();
-
-        foreach ($view_paths as $path) {
+        // Process view paths
+        foreach ($this->laravel['view']->getFinder()->getPaths() as $path) {
             $this->processPath($path);
         }
 
-        foreach ($namespace_paths as $namespace => $paths) {
+        // Process packages
+        foreach ($this->laravel['view']->getFinder()->getHints() as $namespace => $paths) {
             foreach ($paths as $path) {
                 $this->processPath($path, $namespace);
             }
@@ -50,6 +59,13 @@ class CompileCommand extends Command
         $this->info($this->count.' Twig templates compiled');
     }
 
+    /**
+     * Compiles all Twig files under path to cache directory.
+     *
+     * @param string $path      All twig files under this path are compiled.
+     * @param string $namespace If path is part of a package, then the name-space hint.
+     * @return void
+     */
     protected function processPath($path, $namespace = null)
     {
         $path = realpath($path);
