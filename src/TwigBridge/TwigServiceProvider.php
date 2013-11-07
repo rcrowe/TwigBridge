7ab0f03528ed90079ef81d3fbb3b3e17ed30b5d6
Rob Crowe <hello@vivalacrowe.com>
2013-11-07 21:47:55 +0000

    Inital commit

---
+++ src/TwigBridge/TwigServiceProvider.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Brings Twig to Laravel 4.
+ *
+ * @author Rob Crowe <hello@vivalacrowe.com>
+ * @license MIT
+ */
+
+namespace TwigBridge;
+
+use Illuminate\View\ViewServiceProvider;
+use Twig_Loader_Chain;
+
+/**
+ * Bootstrap TwigBridge with Laravel.
+ */
+class TwigServiceProvider extends ViewServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        // Register the package configuration with the loader.
+        $this->app['config']->package('rcrowe/twigbridge', __DIR__.'/../config');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->registerTwigEngine();
+    }
+
+    public function registerTwigEngine()
+    {
+        $app = $this->app;
+
+        // Extensions
+        $app['twig.extensions'] = $app->share(function() use ($app) {
+            $extensions = $app['config']->get('twigbridge::extensions.enabled', array());
+
+            // Is debug enabled?
+            $options = $app['twig.options'];
+            $debug   = (bool) (isset($options['debug'])) ? $options['debug'] : false;
+
+            if ($debug) {
+                array_unshift($extensions, 'Twig_Extension_Debug');
+            }
+
+            return $extensions;
+        });
+
+        // Loader
+        $app['twig.loader.path'] = $app->share(function() {
+            return new Twig\Loader\Path;
+        });
+
+        $app['twig.loader.viewfinder'] = $app->share(function() use ($app) {
+            return new Twig\Loader\Viewfinder(
+                $app['view']->getFinder(),
+                $app['twig.bridge']->getExtension()
+            );
+        });
+
+        $app['twig.loader.filesystem'] = $app->share(function() use ($app) {
+            return new Twig\Loader\Filesystem(
+                $app['view']->getFinder(),
+                $app['twig.bridge']->getExtension()
+            );
+        });
+
+        $app['twig.loader'] = $app->share(function() use ($app) {
+            return new Twig_Loader_Chain(array(
+                $app['twig.loader.path'],
+                $app['twig.loader.viewfinder'],
+                $app['twig.loader.filesystem'],
+            ));
+        });
+
+        // Twig
+        $app['twig.options'] = $app->share(function() use ($app) {
+            return $app['config']->get('twigbridge::twig', array());
+        });
+
+        $app['twig.bridge'] = $app->share(function() use ($app) {
+            return new TwigBridge($app);
+        });
+
+        $app['twig'] = $app->share(function() use ($app) {
+            return $app['twig.bridge']->getTwig();
+        });
+
+        // Engine
+        $app['twig.engine'] = $app->share(function() use ($app) {
+            return new Engine\Twig(
+                $app['twig'],
+                $app['config']->get('twigbridge::globals', array())
+            );
+        });
+
+        $app['view']->addExtension(
+            $app['config']->get('twigbridge::extension', 'twig'),
+            'twig',
+            function() use ($app) {
+                return $app['twig.engine'];
+            }
+        );
+    }
+}
---
+++ tests/TwigBridgeTests/ServiceProviderTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace TwigBridgeTests;
+
+use PHPUnit_Framework_TestCase;
+use Mockery as m;
+use Illuminate\Foundation\Application;
+use Illuminate\View\Environment;
+use Illuminate\Config\Repository;
+
+use TwigBridge\TwigServiceProvider;
+
+class ServiceProviderTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testBound()
+    {
+        $bindings = array(
+            'twig.extensions',
+            'twig.loader.path',
+            'twig.loader.viewfinder',
+            'twig.loader.filesystem',
+            'twig.loader',
+            'twig.options',
+            'twig.bridge',
+            'twig',
+            'twig.engine',
+        );
+
+        $app      = $this->getApplication();
+        $provider = new TwigServiceProvider($app);
+
+        // Make sure not found
+        foreach ($bindings as $binding) {
+            $this->assertFalse($app->bound($binding));
+        }
+
+        // Boot provider
+        $provider->boot();
+
+        // Now make sure bounded
+        foreach ($bindings as $binding) {
+            $this->assertTrue($app->bound($binding));
+        }
+    }
+
+    protected function getApplication()
+    {
+        $app = new Application;
+        $app->instance('path', __DIR__);
+
+        $app['env']          = 'production';
+        $app['path.storage'] = __DIR__.'/storage';
+
+        // View
+        $finder = m::mock('Illuminate\View\ViewFinderInterface');
+        $finder->shouldReceive('addExtension');
+
+        $engine = m::mock('Illuminate\View\Engines\EngineResolver');
+        $engine->shouldReceive('register');
+
+        $app['view'] = new Environment(
+            $engine,
+            $finder,
+            m::mock('Illuminate\Events\Dispatcher')
+        );
+
+        // Config
+        $config = new Repository(m::mock('Illuminate\Config\LoaderInterface'), 'production');
+
+        $config->getLoader()->shouldReceive('addNamespace')->with('twigbridge', __DIR__);
+        $config->getLoader()->shouldReceive('cascadePackage')->andReturnUsing(function($env, $package, $group, $items) { return $items; });
+        $config->getLoader()->shouldReceive('exists')->with('extension', 'twigbridge')->andReturn(false);
+        $config->getLoader()->shouldReceive('exists')->with('extensions', 'twigbridge')->andReturn(false);
+        $config->getLoader()->shouldReceive('exists')->with('delimiters', 'twigbridge')->andReturn(false);
+        $config->getLoader()->shouldReceive('exists')->with('twig', 'twigbridge')->andReturn(false);
+        $config->getLoader()->shouldReceive('load')->with('production', 'config', 'twigbridge')->andReturn(
+            array(
+                'extension'  => 'twig',
+                'twig'       => array(),
+                'extensions' => array(
+                    'TwigBridge\Extension\Dummy',
+                )
+            )
+        );
+
+        $config->package('foo/twigbridge', __DIR__);
+        $app['config'] = $config;
+
+        return $app;
+    }
+}
