b8898526af6808d3b1565837a80d02fa998dcd86
Rob Crowe <hello@vivalacrowe.com>
2014-03-21 15:08:50 +0000

    Fully test the service provider

---
+++ tests/ServiceProvider/Bindings/CommandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TwigBridge\Tests\ServiceProvider\Bindings;
+
+use TwigBridge\Tests\Base;
+use TwigBridge\ServiceProvider;
+
+class CommandTest extends Base
+{
+    protected $app;
+
+    public function setUp()
+    {
+        $this->app = $this->getApplication();
+
+        $provider = new ServiceProvider($this->app);
+        $provider->boot();
+    }
+
+    public function testTwigInstance()
+    {
+        $this->assertEquals(get_class($this->app['command.twig']), 'TwigBridge\Command\TwigBridge');
+    }
+
+    public function testCleanInstance()
+    {
+        $this->assertEquals(get_class($this->app['command.twig.clean']), 'TwigBridge\Command\Clean');
+    }
+
+    public function testLintInstance()
+    {
+        $this->assertEquals(get_class($this->app['command.twig.lint']), 'TwigBridge\Command\Lint');
+    }
+}
