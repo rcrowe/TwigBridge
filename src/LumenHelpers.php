949069840794fbd9e4bbb6541323e846a5865c01
Patrick Douglas <patrick@engrain.com>
2015-05-15 11:38:24 -0600

    Add config_path helper for Lumen

---
+++ src/LumenHelpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( ! function_exists('config_path'))
+{
+    /**
+     * Get the configuration path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return base_path('config' . ($path ? '/'.$path : $path));
+    }
+}
