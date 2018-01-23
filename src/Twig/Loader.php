826f7badc31a0746444459deb485508c66da669b
its <itsgoingd@luzer.sk>
2018-01-23 21:45:58 +0100

    Removed deprecated method.

--- src/Twig/Loader.php
+++ src/Twig/Loader.php
@@ -113,20 +113,6 @@ class Loader implements Twig_LoaderInterface
     }
 
     /**
-     * @param string $name
-     *
-     * @deprecated Will be dropped with support of 1.x in favour of getSourceContext()
-     *
-     * @return string
-     */
-    public function getSource($name)
-    {
-        $path = $this->findTemplate($name);
-
-        return $this->files->get($path);
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function getSourceContext($name)
