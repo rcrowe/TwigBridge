d484a72256f8a3a6f308d5150f489eafbe926ee6
William Hall <william.hall@synergitech.co.uk>
2018-12-18 09:53:52 +0000

    repackage

--- src/Command/Lint.php
+++ src/Command/Lint.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of the TwigBridge package.
- *
- * @copyright Robert Crowe <hello@vivalacrowe.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace TwigBridge\Command;
 
 use Illuminate\Console\Command;
@@ -389,10 +380,5 @@ class Lint extends Command
                 'text',
             ],
         ];
-    }
-
-    public function fire()
-    {
-        return $this->handle();
     }
 }
--- src/Engine/Twig.php
+++ src/Engine/Twig.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of the TwigBridge package.
- *
- * @copyright Robert Crowe <hello@vivalacrowe.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace TwigBridge\Engine;
 
 use Illuminate\View\Engines\CompilerEngine;
