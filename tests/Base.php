cfa9a118107a9053ae88388f766e261743e93fb3
Philippe Ouellet <filcius@users.noreply.github.com>
2020-08-12 13:53:48 -0400

    Add tests to assert how the Laravel Events affect template rendering (#395)
    * Add tests to assert how the Laravel Events affect template rendering
    
    * TwigBridgeTestTrait->getApplication : remove extra $extensions parameter
    
    * Fix phpCs style
    
    * Added missing parent setup() call

--- tests/Base.php
+++ tests/Base.php
@@ -3,82 +3,8 @@
 namespace TwigBridge\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Mockery as m;
-use Illuminate\Foundation\Application;
-use Illuminate\View\Factory;
-use Illuminate\Config\Repository;
-use Illuminate\View\Engines\EngineResolver;
 
 abstract class Base extends TestCase
 {
-    protected $twigBridgeRoot;
-
-    public function __construct($name = null, array $data = [], $dataName = '')
-    {
-        parent::__construct($name, $data, $dataName);
-
-        $this->twigBridgeRoot = realpath(__DIR__ . '/../src');
-    }
-
-    public function tearDown()
-    {
-        m::close();
-    }
-
-    protected function getApplication(array $customConfig = [])
-    {
-        $app = new Application;
-        $app->instance('path', __DIR__);
-
-        $app['env'] = 'production';
-        $app['path.config'] = __DIR__ . '/config';
-        $app['path.storage'] = __DIR__ . '/storage';
-
-        // Filesystem
-        $files = m::mock('Illuminate\Filesystem\Filesystem');
-        $app['files'] = $files;
-
-        // View
-        $finder = m::mock('Illuminate\View\ViewFinderInterface');
-        $finder->shouldReceive('addExtension');
-
-        $app['view'] = new Factory(
-            new EngineResolver,
-            $finder,
-            m::mock('Illuminate\Events\Dispatcher')
-        );
-
-        $config = include $this->twigBridgeRoot . '/../config/twigbridge.php';
-
-        $configData = [
-            'twigbridge' => [
-                'extensions' => $config['extensions'],
-                'twig'       => [
-                    'extension'   => 'twig',
-                    'environment' => [
-                        'debug'               => false,
-                        'charset'             => 'utf-8',
-                        'base_template_class' => 'TwigBridge\Twig\Template',
-                        'cache'               => null,
-                        'auto_reload'         => true,
-                        'strict_variables'    => false,
-                        'autoescape'          => true,
-                        'optimizations'       => -1,
-                    ],
-                    'globals'     => [],
-                ],
-            ],
-        ];
-
-        $configData['twigbridge'] = array_replace_recursive($configData['twigbridge'], $customConfig);
-
-        // Config
-        $app['config'] = new Repository($configData);
-
-        $app->bind('Illuminate\Config\Repository', function () use ($app) {
-            return $app['config'];
-        });
-
-        return $app;
-    }
+    use TwigBridgeTestTrait;
 }
---
+++ tests/TwigBridgeTestTrait.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace TwigBridge\Tests;
+
+use Illuminate\Config\Repository;
+use Illuminate\Foundation\Application;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Factory;
+use Mockery as m;
+use TwigBridge\ServiceProvider;
+
+trait TwigBridgeTestTrait
+{
+    protected $twigBridgeRoot;
+
+
+    public function setup()
+    {
+        $this->twigBridgeRoot = realpath(__DIR__ . '/../src');
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /**
+     * @param array $customConfig that will override the default config.
+     *      A recursive merge is apply except for $customConfig['extensions'] which
+     *      will replace the whole 'extensions' if present
+     * @return Application
+     */
+    protected function getApplication(array $customConfig = [])
+    {
+        $app = new Application;
+        $app->instance('path', __DIR__);
+
+        $app['env'] = 'production';
+        $app['path.config'] = __DIR__ . '/config';
+        $app['path.storage'] = __DIR__ . '/storage';
+
+        // Filesystem
+        $files = m::mock('Illuminate\Filesystem\Filesystem');
+        $app['files'] = $files;
+
+        // View
+        $finder = m::mock('Illuminate\View\ViewFinderInterface');
+        $finder->shouldReceive('addExtension');
+
+        $app['view'] = new Factory(
+            new EngineResolver,
+            $finder,
+            m::mock('Illuminate\Events\Dispatcher')
+        );
+
+        if (!isset($customConfig['extensions'])) {
+            $config = include $this->twigBridgeRoot . '/../config/twigbridge.php';
+            $customConfig['extensions'] = $config['extensions'];
+        }
+
+        $configData = [
+            'twigbridge' => [
+                'twig' => [
+                    'extension'   => 'twig',
+                    'environment' => [
+                        'debug'               => false,
+                        'charset'             => 'utf-8',
+                        'base_template_class' => 'TwigBridge\Twig\Template',
+                        'cache'               => null,
+                        'auto_reload'         => true,
+                        'strict_variables'    => false,
+                        'autoescape'          => true,
+                        'optimizations'       => -1,
+                    ],
+                    'globals'     => [],
+                ],
+            ],
+        ];
+
+        $configData['twigbridge'] = array_replace_recursive($configData['twigbridge'], $customConfig);
+
+        // Config
+        $app['config'] = new Repository($configData);
+
+        $app->bind('Illuminate\Config\Repository', function () use ($app) {
+            return $app['config'];
+        });
+
+        return $app;
+    }
+
+    protected function addBridgeServiceToApplication(Application $app)
+    {
+        $provider = new ServiceProvider($app);
+
+        // Register and boot provider
+        $provider->register();
+        $provider->boot();
+    }
+}
