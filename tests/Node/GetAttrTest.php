f0249fe9a0aeba13be55dbe60f93f3d11c0a0bdd
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2020-10-14 15:20:02 +0200

    Add test to ensure that the model exists method can be used (#406)

--- tests/Node/GetAttrTest.php
+++ tests/Node/GetAttrTest.php
@@ -46,6 +46,13 @@ class GetAttrTest extends NodeTestCase
         $object->attr = null;
         $actual = GetAttrNode::attribute($twig, $template->getSourceContext(), $object, "attr", [], Template::ANY_CALL);
         $this->assertEquals(null, $actual);
+
+        $actual = GetAttrNode::attribute($twig, $template->getSourceContext(), $object, "exists", [], Template::ANY_CALL);
+        $this->assertEquals(false, $actual);
+
+        $object->exists = true;
+        $actual = GetAttrNode::attribute($twig, $template->getSourceContext(), $object, "exists", [], Template::ANY_CALL);
+        $this->assertEquals(true, $actual);
     }
 
     public function testGetAttributeOnModelWithSandbox()
