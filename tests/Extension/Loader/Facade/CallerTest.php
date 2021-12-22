48306ca95c313528e684f4f1f23d4fc97fa08525
bytestream <kieran@supportpal.com>
2021-12-22 00:55:57 +0000

    Fix PHP 8 TypeError

--- tests/Extension/Loader/Facade/CallerTest.php
+++ tests/Extension/Loader/Facade/CallerTest.php
@@ -2,8 +2,9 @@
 
 namespace TwigBridge\Tests\Extension\Loader\Facade;
 
+use Illuminate\Support\Facades\Config;
+use Twig\Markup;
 use TwigBridge\Tests\Base;
-use Mockery as m;
 use TwigBridge\Extension\Loader\Facade\Caller;
 
 class CallerTest extends Base
@@ -18,5 +19,23 @@ class CallerTest extends Base
             'charset' => null,
             'name'    => 'Rob',
         ]);
+    }
+
+    public function testSafeMagicMethods(): void
+    {
+        $app = $this->getApplication();
+        Config::setFacadeApplication($app);
+        $caller = new Caller(Config::class, ['is_safe' => true]);
+
+        $this->assertNotInstanceOf(Markup::class, $caller->all());
+    }
+
+    public function testUnsafeMagicMethods(): void
+    {
+        $app = $this->getApplication();
+        Config::setFacadeApplication($app);
+        $caller = new Caller(Config::class, ['is_safe' => true]);
+
+        $this->assertInstanceOf(Markup::class, $caller->get('twigbridge.twig.extension'));
     }
 }
